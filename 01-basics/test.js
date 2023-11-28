61abe0ba1e48fc10b91545315fb84a6bfe2cb8e6
rajatrjrk <152277740+rajatrjrk@users.noreply.github.com>
2023-11-28 12:00:29 +0000

    test file added

---
+++ 01-basics/test.js
@@ -0,0 +1,9 @@
+console.log("hey Webbocket");
+/*
+1-javascript is a high level scripting language used to devlop web and mobile application efficiently.
+2-we used javascript in client side as well as server side programming language.
+3-javascript was devlopped to write logic/functionality to our code to perform everywhere.
+4-javascript was devlopped in 1997 and the 1st version ES1 and later it was ES2,ES3--------
+5-now we used ES6 version after 2015 ES6-Echma script 6(vanilla javascript).
+
+*/
